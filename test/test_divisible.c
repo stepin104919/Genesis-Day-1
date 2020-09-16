69fc063a7feb7ba428367b3eb866daf8bd719e4d
Anand A Warrier <69119987+stepin104919@users.noreply.github.com>
2020-09-17 01:33:45 +0530

    Update test_divisible.c

--- test/test_divisible.c
+++ test/test_divisible.c
@@ -1,40 +1,71 @@
-	#include "unity.h"
-	#include "divisible.h"
+#include <CUnit/Basic.h>
+#include <CUnit/CUnit.h>
 
-	void setUp()
-	{	}
-	void tearDown()
-	{	}
+/* Modify these two lines according to the project */
+#include <divisible.h>
+#define PROJECT_NAME    "Divisible"
 
-	void test_divisiblebyzero(void)
-	{
-  		TEST_ASSERT_EQUAL(-1, divisible(6, 0));
-  		TEST_ASSERT_EQUAL(-1, divisible(25, 0));
-	}
-	void test_perfectlydivisible(void)
-	{
-	    TEST_ASSERT_EQUAL(1, divisible(20, 5));
-	    TEST_ASSERT_EQUAL(1, divisible(48, 12));
-	}
-	void test_notdivisible(void)
-	{
-	    TEST_ASSERT_EQUAL(0, divisible(71, 4));
-	    TEST_ASSERT_EQUAL(0, divisible(50, 3));
-	}
-	void test_divisiblebynegative(void)
-	{
-	    TEST_ASSERT_EQUAL(1, divisible(-32, 16));
-		TEST_ASSERT_EQUAL(0, divisible(4, -3));
-	}
+/* Prototypes for all the test functions */
+void test_divisiblebyzero(void);
+void test_perfectlydivisible(void);
+void test_notdivisible(void);
+void test_divisiblebynegative(void);
 
-	int test_main(void)
-	{
-	  	UNITY_BEGIN();
+/* Start of the application test */
+int main() {
+/* Note: Do not edit START*/
+  /*Initialize and setup the Test Framework */
+  if (CUE_SUCCESS != CU_initialize_registry())
+    return CU_get_error();
+  CU_pSuite suite = CU_add_suite(PROJECT_NAME, 0, 0);
+/* Note: Do not edit END */
+  
+  
+  /* Add your test functions in this format for testing*/
+  CU_add_test(suite, "divisiblebyzero", test_divisiblebyzero);
+  CU_add_test(suite, "perfectlydivisible", test_perfectlydivisible);
+  CU_add_test(suite, "notdivisible", test_notdivisible);
+  CU_add_test(suite, "divisiblebynegative", test_divisiblebynegative);
 
-	  	RUN_TEST(test_divisiblebyzero);
-	  	RUN_TEST(test_perfectlydivisible);
-	  	RUN_TEST(test_notdivisible);
-	  	RUN_TEST(test_divisiblebynegative);
 
-  		return UNITY_END();
-	}
+/* Note: Do not edit START*/
+  /* Setup Test Framework to output the result to Screen */
+  CU_basic_set_mode(CU_BRM_VERBOSE);
+  
+  /* run the unit test framework*/
+  CU_basic_run_tests();
+  
+  /* Cleaning the Resources used by Unit test framework */
+  CU_cleanup_registry();
+/* Note: Do not edit END */
+  return 0;
+}
+
+/* Write all the test functions */ 
+void test_divisiblebyzero(void) {
+  CU_ASSERT(-1 == divisiblebyzero(6, 0));
+  
+  /* Dummy fail*/
+  CU_ASSERT(1 == divisiblebyzero(25, 0));
+}
+
+void test_perfectlydivisible(void) {
+  CU_ASSERT(1 == perfectlydivisible(20, 5));
+  
+  /* Dummy fail*/
+  CU_ASSERT(0 == perfectlydivisible(48, 12));
+}
+
+void test_notdivisible(void) {
+  CU_ASSERT(0 == notdivisible(71, 4));
+  
+  /* Dummy fail*/
+  CU_ASSERT(-1 == notdivisible(50, 3));
+}
+
+void test_divisiblebynegative(void) {
+  CU_ASSERT(1 == divisiblebynegative(-32, 16));
+  
+  /* Dummy fail*/
+  CU_ASSERT(1 == divisiblebynegative(4, -3));
+}
